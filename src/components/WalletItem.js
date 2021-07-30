3d7fba0dd00e2306422fb8738fe3f10a58c626ec
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-07-30 12:38:49 +0100

    added style to wallet card

--- src/components/WalletItem.js
+++ src/components/WalletItem.js
@@ -1,9 +1,14 @@
+import './WalletItem.css';
 import React from 'react';
 
 const walletItem = () => {
   return (
-    <h2>Wallet item</h2>
-  )
+    <div className='wallet-item'>
+        <div className='wallet-item-date'>6th July 2021</div>
+        <h2>Waitrose Shop</h2>
+      <div className='wallet-item-price'>£39.93</div>
+    </div>
+  );
 }
 
 export default walletItem;
