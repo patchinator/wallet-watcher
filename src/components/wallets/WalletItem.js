b6f9a9333aebebb7711e67158622930d62d86f55
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-08-05 16:38:04 +0100

    updated style and font size

--- src/components/wallets/WalletItem.js
+++ src/components/wallets/WalletItem.js
@@ -6,10 +6,12 @@ import Card from '../UI/Card';
 const WalletItem = (props) => {
 
   return (
-    <Card className='wallet-item'>
-        <WalletDate date={props.date} />
+    <Card className="wallet-item">
+      <WalletDate date={props.date} />
+      <div className="wallet-item-info">
         <h2>{props.title}</h2>
-      <div className='wallet-item-price'>£{props.amount}</div>
+        <div className='wallet-item-price'>£{props.amount}</div>
+      </div>
     </Card>
   );
 }
